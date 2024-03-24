4dfd2435bd6ff856131450909d7a76b12f21fb0c
preksha <jainpreksha2712@gmail.com>
2024-03-24 19:39:05 +0530

    create schema models

---
+++ src/Models/message.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const MessageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model("Message", MessageSchema);
+
+module.exports = Message;
---
+++ src/Models/user.model.js
@@ -0,0 +1,40 @@
+const { default: mongoose } = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+    },
+    avatar: {
+      type: String,
+    },
+    lastSeen: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
