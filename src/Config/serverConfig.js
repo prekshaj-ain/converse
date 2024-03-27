02564728db05339ae44f63af081e0b91d6e24b90
preksha <jainpreksha2712@gmail.com>
2024-03-27 16:20:15 +0530

    configure env variables

--- src/Config/serverConfig.js
+++ src/Config/serverConfig.js
@@ -6,4 +6,8 @@ module.exports = {
   CORS_ORIGIN: process.env.CORS_ORIGIN,
   MONGODB_URI: process.env.MONGODB_URI,
   DB_NAME: process.env.DB_NAME,
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+  JWT_ACCESS_EXPIRY: process.env.JWT_ACCESS_EXPIRY,
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+  JWT_REFRESH_EXPIRY: process.env.JWT_REFRESH_EXPIRY,
 };
