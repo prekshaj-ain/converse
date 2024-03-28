041dc5b9125fc4b2882137d0ddc5749b3ecb0176
preksha <jainpreksha2712@gmail.com>
2024-03-28 17:44:59 +0530

    configure passport

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const { CORS_ORIGIN } = require("./Config/serverConfig");
+const passport = require("passport");
 
 const app = express();
 
@@ -15,5 +16,11 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+
+app.use(passport.initialize());
+require("./Service/facebookStrategy");
+require("./Service/googleStrategy");
+require("./Service/jwtStrategy");
+require("./Service/localStrategy");
 
 module.exports = app;
