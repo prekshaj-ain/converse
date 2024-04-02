83995e1a292cb138f5feecc1d404e5eb3581ea82
preksha <jainpreksha2712@gmail.com>
2024-04-02 17:08:01 +0530

    add google and github passport strategy

--- src/Routes/Auth/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const facebookAuth = require("./facebookAuth");
-const googleAuth = require("./googleAuth");
-const router = Router();
-
-router.use("/facebook", facebookAuth);
-router.use("/google", googleAuth);
-
-module.exports = router;
