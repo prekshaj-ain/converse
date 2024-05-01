85918952adb3be404061400cc21629902f35cb55
preksha <jainpreksha2712@gmail.com>
2024-05-01 10:52:45 +0530

    change app to httpServer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const connectDB = require("./Config/databaseConfig");
 const { PORT } = require("./Config/serverConfig");
-const app = require("./app");
+const httpServer = require("./app");
 
 connectDB()
   .then(() => {
-    app.listen(PORT || 8000, () => {
+    httpServer.listen(PORT || 8000, () => {
       console.log(`⚙️ Server is running at port : ${PORT}`);
     });
   })
